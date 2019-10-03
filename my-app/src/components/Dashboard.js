1c796a57a178ea63ec5713f916779d924325b1b8
Jnmendza <jonatmendoza@gmail.com>
2019-10-03 12:51:44 -0700

    Dashboard and Display files in component folder-setup completed

---
+++ my-app/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            This is the Dashboard:
+            <button>Strikes</button>
+            <button>Balls</button>
+
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ my-app/src/components/Display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Display = () => {
+    return(
+        <div>
+            This is the Display: Strikes and Balls
+        </div>
+    )
+}
+
+export default Display;
